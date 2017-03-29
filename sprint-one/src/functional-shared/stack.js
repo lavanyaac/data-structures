a49af1b2032a267a20d47f201d688e0843f71e1c
Hack Reactor Student <student@hackreactor.com>
2017-03-29 15:56:06 -0700

    Implemented stack - The-shared pattern.

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  extend(someInstance, stackMethods);
+  
+  return someInstance;
 };
 
+var extend = function(obj1, obj2){
+	return Object.assign(obj1, obj2)
+}
+
+
 var stackMethods = {};
+
+stackMethods.size = function(){
+	return Object.keys(this.storage).length;
+}
+
+stackMethods.push = function(value){
+	var key = this.size() + 1;
+    this.storage[key] = value;
+}
+
+stackMethods.pop = function() {
+	var popItem;
+	if(this.size() > 0){
+		popItem = this.storage[this.size()];
+		delete this.storage[this.size()];
+	}
+	return popItem;
+}
 
 
