1edfa806af3baa634eacd95a648736f99d3ea0f5
Hack Reactor Student <student@hackreactor.com>
2017-03-30 11:17:31 -0700

    Implemented Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    
+    var linkedListNode = new Node(value);
+    if ( list.head === null ){
+      list.head = linkedListNode;
+    }
+    if ( list.tail !== null ) {
+      list.tail.next = linkedListNode;
+    }
+    
+    list.tail = linkedListNode;
+
+
   };
 
   list.removeHead = function() {
+    var listHead = list.head;
+    list.head = list.head.next;
+    return listHead.value;
+
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while ( currentNode ) {
+      if ( currentNode.value === target ) {
+        return true;
+      }
+      else {
+        currentNode = currentNode.next;
+      }
+    }
+   return false;
   };
 
   return list;
@@ -26,4 +52,6 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ constant time to add or remove items from linked list
+ linear time to check contains of linked list
  */
