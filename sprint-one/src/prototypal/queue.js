651de68b97855a365d1b3a451633bd5f6ee8f297
Hack Reactor Student <student@hackreactor.com>
2017-03-30 10:40:20 -0700

    updating prototypal/queue.js          pseudoclassical/queue.js

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
 var queueMethods = {};
+
+queueMethods.size = function(){
+	return Object.keys(this.storage).length;
+};
+
+queueMethods.enqueue = function(value) {
+	var keyIn;
+	if ( this.size() === 0 ) {
+		keyIn = 1;
+	}
+	else {
+		keyIn = Math.max.apply(null, Object.keys(this.storage)) + 1;
+	}
+	this.storage[keyIn] = value;
+};
+
+queueMethods.dequeue = function() {
+  var keyOut = Math.min.apply(null, Object.keys(this.storage) );
+  var dequeueItem = this.storage[keyOut];
+  delete this.storage[keyOut];
+  return dequeueItem; 
+};
+
+
 
 
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+
 };
+
+Queue.prototype.size = function() {
+	return Object.keys(this.storage).length;
+};
+
+Queue.prototype.enqueue = function(value) {
+	var keyIn;
+	if (this.size() === 0 ) {
+		keyIn = 1;
+	}
+	else {
+		keyIn = Math.max.apply(null, Object.keys(this.storage)) + 1;
+	}
+	this.storage[keyIn] = value;
+};
+
+Queue.prototype.dequeue = function() {
+	var keyOut = Math.min.apply(null, Object.keys(this.storage));
+	var dequeueItem = this.storage[keyOut];
+	delete this.storage[keyOut];
+	return dequeueItem;
+}
 
 
