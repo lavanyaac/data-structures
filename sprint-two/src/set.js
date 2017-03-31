13e4462d3668eacb95168d0640ca0f934da159be
Hack Reactor Student <student@hackreactor.com>
2017-03-30 19:55:35 -0700

    updated Set function expression

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+	if (this._storage.includes(item) === false){
+		this._storage.push(item);
+	}
 };
 
 setPrototype.contains = function(item) {
+	return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
+	var idx = this._storage.indexOf(item);
+	if(idx >= 0 ){
+		this._storage =  this._storage.slice(0, idx-1).concat(this._storage.slice(idx+1))
+	}
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ add = O(n)
+ contains = O(n)
+ remove = O(n)
  */
